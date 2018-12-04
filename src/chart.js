b3c0865edd336ad20a3421ec55b3687f0a61c434
Wen Tjun <wentjun289@hotmail.com>
2018-12-05 01:53:40 +0800

    removed additional console.log statements

--- src/chart.js
+++ src/chart.js
@@ -17,13 +17,12 @@ const getData = loadJson('./sti-full.json', text => {
 });
 */
 
-const movingAverage = (data, neighbors) => {
+const movingAverage = (data, numberOfPricePoints) => {
   return data.map((row, index, total) => {
-    const start = Math.max(0, index - neighbors);
-    //const end = index + neighbors;
+    const start = Math.max(0, index - numberOfPricePoints);
+    //const end = index + numberOfPricePoints;
     const end = index;
     const subset = total.slice(start, end + 1);
-    console.log(row, subset);
     const sum = subset.reduce((a, b) => {
       return a + b.close;
     }, 0);

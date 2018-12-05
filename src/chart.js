d68be5baba6274ddd61575a3a1c2caa8712aeb22
Wen Tjun <wentjun289@hotmail.com>
2018-12-06 02:49:50 +0800

    renamed mouseover variables
    - renamed variables in mouseover function for meaningfulness
    - changed sample data file name

--- src/chart.js
+++ src/chart.js
@@ -11,7 +11,7 @@ const loadJson = (file, callback) => {
   xobj.send(null);
 };
 /*
-const getData = loadJson('./sti-full.json', text => {
+const getData = loadJson('./sample-data.json', text => {
   const data = JSON.parse(text);
   console.log(data);
 });
@@ -34,7 +34,7 @@ const movingAverage = (data, numberOfPricePoints) => {
   });
 };
 
-const loadData = d3.json('sti-full.json').then(data => {
+const loadData = d3.json('sample-data.json').then(data => {
   const chartResultsData = data['chart']['result'][0];
   const quoteData = chartResultsData['indicators']['quote'][0];
 
@@ -99,27 +99,6 @@ const initialiseChart = data => {
     .domain([yMin, yMax])
     .range([height, 0]);
 
-  // generates lines when called
-  const line = d3
-    .line()
-    .x(d => {
-      return xScale(d['date']);
-    })
-    .y(d => {
-      return yScale(d['close']);
-    });
-
-  let movingSum;
-  const movingAverageLine = d3
-    .line()
-    .x(d => {
-      return xScale(d['date']);
-    })
-    .y(d => {
-      return yScale(d['average']);
-    })
-    .curve(d3.curveBasis);
-
   // add chart SVG to the page
   const svg = d3
     .select('#chart')
@@ -142,7 +121,28 @@ const initialiseChart = data => {
     .attr('transform', `translate(${width}, 0)`)
     .call(d3.axisRight(yScale));
 
-  // render lines
+  // renders close price line chart and moving average line chart
+
+  // generates lines when called
+  const line = d3
+    .line()
+    .x(d => {
+      return xScale(d['date']);
+    })
+    .y(d => {
+      return yScale(d['close']);
+    });
+
+  const movingAverageLine = d3
+    .line()
+    .x(d => {
+      return xScale(d['date']);
+    })
+    .y(d => {
+      return yScale(d['average']);
+    })
+    .curve(d3.curveBasis);
+
   svg
     .append('path')
     .data([data]) // binds data to the line
@@ -168,11 +168,8 @@ const initialiseChart = data => {
     .style('display', 'none');
 
   focus.append('circle').attr('r', 4.5);
-
   focus.append('line').classed('x', true);
-
   focus.append('line').classed('y', true);
-
   focus
     .append('text')
     .attr('x', 9)
@@ -185,7 +182,7 @@ const initialiseChart = data => {
     .attr('height', height)
     .on('mouseover', () => focus.style('display', null))
     .on('mouseout', () => focus.style('display', 'none'))
-    .on('mousemove', mousemove);
+    .on('mousemove', generateCrosshair);
 
   d3.select('.overlay').style('fill', 'none');
   d3.select('.overlay').style('pointer-events', 'all');
@@ -195,24 +192,30 @@ const initialiseChart = data => {
   d3.selectAll('.focus line').style('stroke-width', '1.5px');
   d3.selectAll('.focus line').style('stroke-dasharray', '3 3');
 
+  //returs insertion point
   const bisectDate = d3.bisector(d => d.date).left;
 
   /* mouseover function to generate crosshair */
-  function mousemove() {
-    const x0 = xScale.invert(d3.mouse(this)[0]);
-    const i = bisectDate(data, x0, 1);
+  function generateCrosshair() {
+    //returns corresponding value from the domain
+    const correspondingDate = xScale.invert(d3.mouse(this)[0]);
+    //gets insertion point
+    const i = bisectDate(data, correspondingDate, 1);
     const d0 = data[i - 1];
     const d1 = data[i];
-    const d = x0 - d0['date'] > d1['date'] - x0 ? d1 : d0;
+    const currentPoint =
+      correspondingDate - d0['date'] > d1['date'] - correspondingDate ? d1 : d0;
     focus.attr(
       'transform',
-      `translate(${xScale(d['date'])}, ${yScale(d['close'])})`
+      `translate(${xScale(currentPoint['date'])}, ${yScale(
+        currentPoint['close']
+      )})`
     );
 
     focus
       .select('line.x')
       .attr('x1', 0)
-      .attr('x2', width - xScale(d['date']))
+      .attr('x2', width - xScale(currentPoint['date']))
       .attr('y1', 0)
       .attr('y2', 0);
 
@@ -221,9 +224,10 @@ const initialiseChart = data => {
       .attr('x1', 0)
       .attr('x2', 0)
       .attr('y1', 0)
-      .attr('y2', height - yScale(d['close']));
+      .attr('y2', height - yScale(currentPoint['close']));
 
-    updateLegends(d);
+    // updates the legend to display the date, open, close, high, low, and volume and selected mouseover area
+    updateLegends(currentPoint);
   }
 
   /* Legends */
@@ -449,27 +453,34 @@ const setPeriodFilter = filter => {
       .attr('height', height)
       .on('mouseover', () => focus.style('display', null))
       .on('mouseout', () => focus.style('display', 'none'))
-      .on('mousemove', mousemove);
+      .on('mousemove', generateCrosshair);
 
     const focus = d3.select('.focus');
     const bisectDate = d3.bisector(d => d.date).left;
 
     /* mouseover function to generate crosshair */
-    function mousemove() {
-      const x0 = xScale.invert(d3.mouse(this)[0]);
-      const i = bisectDate(res, x0, 1);
-      const d0 = res[i - 1];
-      const d1 = res[i];
-      const d = x0 - d0['date'] > d1['date'] - x0 ? d1 : d0;
+    function generateCrosshair() {
+      //returns corresponding value from the domain
+      const correspondingDate = xScale.invert(d3.mouse(this)[0]);
+      //gets insertion point
+      const i = bisectDate(data, correspondingDate, 1);
+      const d0 = data[i - 1];
+      const d1 = data[i];
+      const currentPoint =
+        correspondingDate - d0['date'] > d1['date'] - correspondingDate
+          ? d1
+          : d0;
       focus.attr(
         'transform',
-        `translate(${xScale(d['date'])}, ${yScale(d['close'])})`
+        `translate(${xScale(currentPoint['date'])}, ${yScale(
+          currentPoint['close']
+        )})`
       );
 
       focus
         .select('line.x')
         .attr('x1', 0)
-        .attr('x2', width - xScale(d['date']))
+        .attr('x2', width - xScale(currentPoint['date']))
         .attr('y1', 0)
         .attr('y2', 0);
 
@@ -478,9 +489,10 @@ const setPeriodFilter = filter => {
         .attr('x1', 0)
         .attr('x2', 0)
         .attr('y1', 0)
-        .attr('y2', height - yScale(d['close']));
+        .attr('y2', height - yScale(currentPoint['close']));
 
-      updateLegends(d);
+      // updates the legend to display the date, open, close, high, low, and volume and selected mouseover area
+      updateLegends(currentPoint);
     }
 
     const updateLegends = currentData => {
